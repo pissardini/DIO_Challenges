b0aaceeefe2c981dc5ecff98fc21f290d6ccd3f8
Rodrigo Pissardini <rodrigo@pissardini.com>
2021-02-19 12:44:53 -0300

    APINet-MongoDB
    Creating initial API with C# and MongoDB connection

---
+++ c_sharp/APINet-MongoDB/Data/MongoDB.cs
@@ -0,0 +1,49 @@
+using System;
+using Api.Data.Collections;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace Api.Data
+{
+    public class MongoDB
+    {
+        public IMongoDatabase DB { get; }
+
+        public MongoDB(IConfiguration configuration)
+        {
+            //pega connection String e faz a conexão
+
+            try
+            {
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(configuration["ConnectionString"]));
+                var client = new MongoClient(settings);
+                DB = client.GetDatabase(configuration["NomeBanco"]); //pegar o banco de dados
+                MapClasses();
+            }
+            catch (Exception ex)
+            {
+                throw new MongoException("It was not possible to connect to MongoDB", ex);
+            }
+        }
+
+        private void MapClasses()
+        {
+            //Entity:
+            // Duas formas: uma com data annotation e outra com automaps
+
+            var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Infectado)))
+            {
+                BsonClassMap.RegisterClassMap<Infectado>(i =>
+                {
+                    i.AutoMap();
+                    i.SetIgnoreExtraElements(true);
+                });
+            }
+        }
+    }
+}
---
+++ c_sharp/APINet-MongoDB/README.md
@@ -0,0 +1,23 @@
+# API.NET with MongoDB
+
+An example of a API based on .NET using MongoDB. This code is based on [gabrielfbarros' lessons](https://github.com/gabrielfbarros/dotnet-mongo) on [Digital Innovation One](https://web.digitalinnovation.one).
+
+##Requirements
+
+* [MongoDB Driver for C#](https://docs.mongodb.com/drivers/csharp/)
+
+## Using 
+
+* This code was created using the resources from [MongoDB Atlas](https://www.mongodb.com/cloud/atlas/register). For initial tests, I recommend that you create an account, start a free cluster and get the connection string. After this, insert the connection string in the file **appsettings.json**.
+
+* For testing, start the API (in VSCode use **dotnet run**). With a tool like Postman, access https://localhost:5001/infectado. If you call the [Post] method you can insert a profile using json: 
+```
+{
+	"dataNascimento": "1990-03-01",
+	"sexo": "M",
+	"latitude": -23.5630994,
+	"longitude": -46.6565712
+}
+```
+
+If you want to get the list of users, you can call the [Get] method using the above endpoint.
